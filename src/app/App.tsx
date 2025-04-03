1ad05ee00b8bd1d65d3d3188e027f88fd577f198
WalexWeb <spomeshchikov@yandex.ru>
2025-04-03 10:26:58 +0300

    feat: added app routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import Registration from "./pages/Registration";
-import StartPage from "./pages/Home";
+import Login from "./pages/Login";
+import News from "./pages/News";
+import Rankings from "./pages/Rankings";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<StartPage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/ranking" element={<Rankings />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </>
   );
