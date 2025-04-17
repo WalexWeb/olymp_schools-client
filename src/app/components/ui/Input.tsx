7f533960d05ccab235a3e1a0f49aa74f9528fb88
WalexWeb <spomeshchikov@yandex.ru>
2025-04-17 13:12:07 +0300

    style: update input

--- src/app/components/ui/Input.tsx
+++ src/app/components/ui/Input.tsx
@@ -1,15 +1,25 @@
 import { ComponentProps } from "react";
 import { fadeUp } from "../animations/fadeUp";
 import { m } from "framer-motion";
+import cn from "clsx";
+import { useThemeStore } from "../../stores/themeStore";
 
 function Input(props: ComponentProps<"input">) {
+  const { isDarkMode } = useThemeStore();
+
   return (
     <m.input
       variants={fadeUp}
       initial="hidden"
       animate="visible"
       {...props}
-      className="rounded-lg border-solid border-blue-500 px-6 py-2 text-lg outline-2 outline-offset-2 outline-blue-500 outline-solid"
+      className={cn(
+        "rounded-lg border-solid border-blue-500 px-6 py-2 text-lg outline-2 outline-offset-2 outline-blue-500 outline-solid",
+        {
+          "placeholder-gray-400": isDarkMode,
+          "bg-blue-200/55 placeholder-gray-500": !isDarkMode,
+        },
+      )}
     />
   );
 }
--- src/app/components/ui/Select.tsx
+++ src/app/components/ui/Select.tsx
@@ -1,20 +1,26 @@
 import { ComponentProps, ReactNode } from "react";
 import { fadeUp } from "../animations/fadeUp";
 import { m } from "framer-motion";
+import cn from "clsx";
+import { useThemeStore } from "../../stores/themeStore";
 
 type SelectProps = ComponentProps<"select"> & {
   children: ReactNode;
 };
 
 function Select({ children, ...props }: SelectProps) {
+  const { isDarkMode } = useThemeStore();
+
   return (
     <m.select
       variants={fadeUp}
       initial="hidden"
       animate="visible"
       {...props}
-      className="rounded-lg border-solid border-blue-500 px-6 py-2 text-lg outline-2 outline-offset-2 outline-blue-500 outline-solid"
-    >
+      className={cn(
+        "rounded-lg border-solid border-blue-500 px-6 py-2 text-lg outline-2 outline-offset-2 outline-blue-500 outline-solid",
+        { "bg-blue-200/55": !isDarkMode },
+      )}    >
       {children}
     </m.select>
   );
