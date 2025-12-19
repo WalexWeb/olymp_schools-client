70544900759de633d94e9d041625e39548957ba3
WalexWeb <spomeshchikov@yandex.ru>
2025-12-19 12:44:29 +0300

    feat: добавил анимацию снега на фон

--- src/app/components/ui/BackgroundBlobs/BackgroundBlobs.tsx
+++ src/app/components/ui/BackgroundBlobs/BackgroundBlobs.tsx
@@ -1,5 +1,6 @@
 import { m } from "framer-motion";
 import { Blob } from "./Blob";
+import Snowfall from "react-snowfall";
 
 export const BackgroundBlobs = () => {
   return (
@@ -8,6 +9,7 @@ export const BackgroundBlobs = () => {
       animate={{ opacity: 1 }}
       className="pointer-events-none fixed inset-0 overflow-hidden"
     >
+      <Snowfall />
       <Blob
         type="rotating"
         className="absolute -top-60 left-1/4 h-[500px] w-[500px] rounded-full bg-gradient-to-r from-blue-900/10 to-purple-900/10 blur-[100px]"
